b56dd318d13c80116301d0a4696401fa56f9a321
Jack Witte <witte.85@osu.edu>
2024-03-24 21:50:03 -0400

    part 3

---
+++ SCHEMING-DEVICE.Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Scheming.Device.Domain.Catalog;
+
+namespace SCHEMING_DEVICE.Data
+{
+    public class StoreContext : DbContext
+{
+        public StoreContext(DbContextOptions<StoreContext> options)
+         : base(options)
+        {}
+
+        public DbSet<Item> Items { get; set;}
+}
+}
